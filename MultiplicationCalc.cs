aeff10e1a6eca325a9e3547a1d69f5d98714e3c7
amyflatt <amyflatt@users.noreply.github.com>
2016-09-18 18:50:36 -0500

    Create MultiplicationCalc.cs
    Works, changes planned,

---
+++ MultiplicationCalc.cs
@@ -0,0 +1,17 @@
+using System;
+
+class MainClass
+{
+	public static void Main(String[]args)
+	{
+		int No1;
+		int No2;
+
+		Console.WriteLine ("Tell me a number and I will multiply it: ");
+		No1 = Convert.ToInt32 (Console.ReadLine ());
+		Console.WriteLine ("Tell me your second number: ");
+		No2 = Convert.ToInt32 (Console.ReadLine ());
+		Console.ReadKey ();
+		
+	}
+}
